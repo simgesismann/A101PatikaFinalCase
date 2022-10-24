035ee6e7e8af2cb24b5cf54d6c768ff6b3cef8d9
simgesismann <62858275+simgesismann@users.noreply.github.com>
2022-10-25 01:04:07 +0300

    testNG tests are done, NEEDS : assertions

--- src/test/java/AddProductsFromDifferentShops.java
+++ src/test/java/AddProductsFromDifferentShops.java
@@ -1,10 +1,14 @@
+import PageObjectModel.Pages.CartPage;
 import PageObjectModel.Pages.HomePage;
+import PageObjectModel.Pages.ProductDetailPage;
 import PageObjectModel.Pages.ProductsPage;
 import org.testng.annotations.Test;
 
 public class AddProductsFromDifferentShops extends BaseTest{
     HomePage homePage;
     ProductsPage productsPage;
+    ProductDetailPage productDetailPage;
+    CartPage cartPage;
     @Test(priority = 1)
     public void typeProductName() {
         homePage = new HomePage(driver);
@@ -24,5 +28,38 @@ public class AddProductsFromDifferentShops extends BaseTest{
         productsPage.assertProductPageIsDirected();
         productsPage.chooseOneProduct();
         System.out.println("choosed");
+    }
+    @Test(priority = 4)
+    public void addProductToCart() throws InterruptedException {
+        productDetailPage = new ProductDetailPage(driver);
+        productDetailPage.windowHandle();
+        productDetailPage.assertProductDetailPageIsDirected();
+        productDetailPage.rollAndClickAddToCartButton();
+        productDetailPage.assertAddCartButtonIsClicked();
+        productDetailPage.clickCloseButton();
+        System.out.println("product added");
+    }
+    @Test(priority = 5)
+    public void addSameProductFromAnotherStoreToCart() throws InterruptedException {
+        productDetailPage = new ProductDetailPage(driver);
+        productDetailPage.assertOtherBuyOptionsTitleIsDisplayed();
+        productDetailPage.getTextTitleOfOtherOptionsList();
+        productDetailPage.clickOtherOptionAddToCartButton();
+        productDetailPage.clickCloseButton();
+        System.out.println("add another");
+    }
+    @Test(priority = 6)
+    public void navigateToCartPage() {
+        homePage = new HomePage(driver);
+        homePage.clickToMyCart();
+        System.out.println("navigated cart page");
+    }
+    @Test(priority = 7)
+    public void confirmChosenProductsAreOnCartPage() {
+        cartPage = new CartPage(driver);
+        cartPage.assertCartPageIsDirected();
+        cartPage.getTextOfOtherFirmTitleInCartPage();
+        cartPage.assertAddedProductFirmIsCorrectInCart();
+        System.out.println("confirmed that products are on page");
     }
 }
