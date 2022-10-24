5006bbd1706704238a15a97471ec336de9c4ec17
simgesismann <62858275+simgesismann@users.noreply.github.com>
2022-10-24 13:28:53 +0300

    step definitions are done basically

---
+++ src/main/java/StepDefinition/addProductsFromDifferentStoresWithoutLoginSteps.java
@@ -0,0 +1,41 @@
+package StepDefinition;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class addProductsFromDifferentStoresWithoutLoginSteps {
+    @Given("navigate to website")
+    public void navigate_to_website() {
+        System.out.println("navigated");
+    }
+    @And("type product name")
+    public void type_product_name() {
+        System.out.println("typed");
+    }
+    @And("search product")
+    public void search_product() {
+        System.out.println("searched");
+    }
+    @And("choose a product")
+    public void choose_a_product() {
+        System.out.println("choosed");
+    }
+    @And("add product to cart")
+    public void add_product_to_cart() {
+        System.out.println("product added");
+    }
+    @And("add same product from another store to cart")
+    public void add_same_product_from_another_store_to_cart() {
+        System.out.println("add another");
+    }
+    @And("navigate to cart page")
+    public void navigate_to_cart_page() {
+        System.out.println("navigated cart page");
+    }
+    @Then("confirm chosen products are on cart page")
+    public void confirm_chosen_products_are_on_cart_page() {
+        System.out.println("confirmed that products are on page");
+    }
+
+}
