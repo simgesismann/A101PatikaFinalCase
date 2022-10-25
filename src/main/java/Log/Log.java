ed99323d109adb3ae1854b4914d74bf36931f135
simgesismann <62858275+simgesismann@users.noreply.github.com>
2022-10-25 09:24:18 +0300

    Log class, log4j.xml file are created

---
+++ src/main/java/Log/Log.java
@@ -0,0 +1,22 @@
+package Log;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
+public class Log {
+    static Logger logger = Logger.getLogger(Log.class);
+
+    public Log(){
+        DOMConfigurator.configure("Log4j.xml");
+    }
+    public void info(String message){
+        logger.info(message);
+    }
+
+    public void warn(String message){
+        logger.warn(message);
+    }
+
+    public void error(String message){
+        logger.error(message);
+    }
+}
