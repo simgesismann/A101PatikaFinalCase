db448cb830909cc57fcddcd21672c09aa2777bb3
simgesismann <62858275+simgesismann@users.noreply.github.com>
2022-10-26 17:49:06 +0300

    search page jaca doc is done

--- src/main/java/PageObjectModel/Pages/SearchBarPage.java
+++ src/main/java/PageObjectModel/Pages/SearchBarPage.java
@@ -8,10 +8,20 @@ public class SearchBarPage extends SearchBarConstants {
     public SearchBarPage(WebDriver driver) {
         super(driver);
     }
+
+    /**
+     * @method "typeProductNameInSearchBarText" is to type a word into search bar.
+     * @param Text is a string type of product name.
+     */
+
     public void typeProductNameInSearchBarText(String Text){
         click(SearchBarTextLocator);
         type(SearchBarTextLocator,Text);
     }
+
+    /**
+     * @method "clickSearchButton" is to click search button.
+     */
     public void clickSearchButton(){
         click(SearchButtonLocator);
     }
