8c2682ab2b9451a71a3d9cf41bed3da5ebbc5c4d
simgesismann <62858275+simgesismann@users.noreply.github.com>
2022-10-28 12:15:23 +0300

    bdd definitions logs re deleted because methods have them now.

--- src/main/java/StepDefinition/addProductsFromDifferentStores.java
+++ src/main/java/StepDefinition/addProductsFromDifferentStores.java
@@ -46,63 +46,46 @@ public class addProductsFromDifferentStores {
     public void confirm_user_is_logged_in() {
         homePage = new HomePage(driver);
         homePage.assertUserLoggedIn();
-        log.info("User is logged in.");
     }
     @And("accept cookies")
     public void acceptCookies(){
         homePage = new HomePage(driver);
         homePage.acceptCookies();
-        log.info("Cookies are accepted.");
     }
     @And("type {string}")
     public void type(String productName) {
         homePage = new HomePage(driver);
         homePage.searchBarPage().typeProductNameInSearchBarText(productName);
-        log = new Log();
-        log.info("Product name is typed in search bar.");
     }
     @And("search product")
     public void search_product() {
         homePage = new HomePage(driver);
         homePage.searchBarPage().clickSearchButton();
-        log = new Log();
-        log.info("Product is searching.");
     }
     @And("choose a product")
     public void choose_a_product() throws InterruptedException {
         productsPage = new ProductsPage(driver);
-        log = new Log();
         productsPage.assertProductPageIsDirected();
-        log.info("Product page is directed.");
         productsPage.chooseOneProduct();
-        log.info("A product from list is chosen.");
     }
     @And("add product to cart")
     public void add_product_to_cart() throws InterruptedException {
         productDetailPage = new ProductDetailPage(driver);
         log = new Log();
         productDetailPage.windowHandle();
-        log.info("Window is handled.");
         productDetailPage.assertProductDetailPageIsDirected();
-        log.info("Product detail page is directed.");
         productDetailPage.rollAndClickAddToCartButton();
         productDetailPage.assertAddCartButtonIsClicked();
-        log.info("Scrolled down and AddToCart Button is clicked");
         productDetailPage.clickCloseButton();
-        log.info("Pop-up is closed");
     }
     @And("add same product from another store to cart")
     public void add_same_product_from_another_store_to_cart() throws InterruptedException {
         productDetailPage = new ProductDetailPage(driver);
-        log = new Log();
         productDetailPage.assertOtherBuyOptionsTitleIsDisplayed();
-        log.info("Other stores options are displayed.");
         productNameInDetailPage = productDetailPage.getTextOfProductName();
         productFirmNameInDetailPage = productDetailPage.getTextTitleOfOtherOptionsList().toUpperCase().replaceAll("\\p{M}", "");
         productDetailPage.clickOtherOptionAddToCartButton();
-        log.info("Other store's AddToCart Button is clicked.");
         productDetailPage.clickCloseButton();
-        log.info("Pop-up is closed");
     }
     @And("navigate to cart page")
     public void navigate_to_cart_page() {
@@ -111,7 +94,6 @@ public class addProductsFromDifferentStores {
         cartPage = new CartPage(driver);
         homePage.clickToMyCartButton();
         cartPage.assertCartPageIsDirected();
-        log.info("Cart Page is directed.");
     }
     @And("confirm product chosen from another store is on cart page")
     public void confirmProductChosenFromAnotherStoreIsOnCartPage(){
@@ -127,8 +109,6 @@ public class addProductsFromDifferentStores {
     public void confirm_chosen_products_are_on_cart_page() {
         cartPage = new CartPage(driver);
         cartPage.assertAddedProductsAreSame();
-        log.info("In cart page, added products are same.");
-
     }
 
     @Then("close all tabs")
